96bb7635044f2b68ea3bac8dc7fdad3f3d082a53
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-08-14 09:06:23 +0300

    change branch to master

---
+++ src/components/Task.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Task({ text, category, onDeleteTask }) {
+  // implementing handle click function
+  function handleClick() {
+    onDeleteTask(text);
+  }
+  return (
+    <div className="task">
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={handleClick} className="delete">
+        X
+      </button>
+    </div>
+  );
+}
+
+export default Task;
---
+++ src/components/TaskList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Task from "./Task"; // importing our Task
+
+//passing on tasks, and onDeleteTask in our function as argument
+
+function TaskList({ tasks, onDeleteTask }) {
+  // mapping our task data
+  const taskList = tasks.map((task) => (
+    <Task
+      key={task.index}
+      text={task.text}
+      category={task.category}
+      onDeleteTask={onDeleteTask}
+    />
+  ));
+  return <div className="tasks">{taskList}</div>;
+}
+
+export default TaskList;
